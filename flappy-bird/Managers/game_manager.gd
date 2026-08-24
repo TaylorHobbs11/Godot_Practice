1d2e45095e68172d94f813b03635d2d00d249e5a
AcinoOmbi <tttahobbs@gmail.com>
2026-08-24 16:17:36 -0500

    Added a transition scene

--- flappy-bird/Managers/game_manager.gd
+++ flappy-bird/Managers/game_manager.gd
@@ -2,9 +2,31 @@ extends Node
 
 const MAIN_SCENE: PackedScene = preload("res://Level/main.tscn")
 const GAME_SCENE: PackedScene = preload("res://Level/game.tscn")
+const LOADING_SCREEN: PackedScene = preload("uid://bh6ruiyour4ei")
+
+
+#region Original
+#func load_game_screen() -> void:
+	#get_tree().change_scene_to_packed(GAME_SCENE)
+#
+#func load_main_screen() -> void:
+	#get_tree().change_scene_to_packed(MAIN_SCENE)
+
+#endregion
+
+#region SimpleTransition
+var _next_scene: PackedScene
+
+func change_to_next() -> void:
+	get_tree().change_scene_to_packed(_next_scene)
 
 func load_game_screen() -> void:
-	get_tree().change_scene_to_packed(GAME_SCENE)
+	_next_scene = GAME_SCENE
+	get_tree().change_scene_to_packed(LOADING_SCREEN)
 
 func load_main_screen() -> void:
-	get_tree().change_scene_to_packed(MAIN_SCENE)
+	_next_scene = MAIN_SCENE
+	get_tree().change_scene_to_packed(LOADING_SCREEN)
+
+
+#endregion
