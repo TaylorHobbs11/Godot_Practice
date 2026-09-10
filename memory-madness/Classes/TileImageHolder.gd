3b7f26b04de39b70da57b866f7747045d7f011d0
TaylorHobbs11 <tttahobbs@gmail.com>
2026-09-09 23:09:09 -0500

    Added an image manager and tween animations for texture 2d on main scene

---
+++ memory-madness/Classes/TileImageHolder.gd
@@ -0,0 +1,3 @@
+class_name TileImageHolder extends Resource
+
+@export var tile_images: Array[Texture2D] = []
--- memory-madness/Globals/ImageMaster.gd
+++ memory-madness/Globals/ImageMaster.gd
@@ -1,11 +1,6 @@
 extends Node
 
+const TILE_IMAGES: TileImageHolder = preload("res://Resources/TileImages.tres")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func get_random_item_image() -> Texture2D:
+	return TILE_IMAGES.tile_images.pick_random()
